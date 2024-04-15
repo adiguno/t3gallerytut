93d5937f64bbad6f00007906670f04089fee7108
Dian <guodian44@gmail.com>
2024-04-15 17:35:11 +0800

    styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,11 +7,20 @@ const imageNames = images.map((number) => number + 1 + ".png");
 export default function HomePage() {
   // console.log(imageNames);
   return (
-    <main className="">
-      <div className="container flex flex-wrap">
-        {imageNames.map((imageName, index) => (
-          <Image width={100} height={100} src={`/${imageName}`} alt=""></Image>
-        ))}
+    <main className="bg-lime-400">
+      <div className="container flex flex-wrap gap-4">
+        {[...imageNames, ...imageNames, ...imageNames].map(
+          (imageName, index) => (
+            <div key={index}>
+              <Image
+                width={100}
+                height={100}
+                src={`/${imageName}`}
+                alt=""
+              ></Image>
+            </div>
+          ),
+        )}
       </div>
     </main>
   );
